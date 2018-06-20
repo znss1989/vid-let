3ef76efdf88cbcd68a0c4cd1c22e36693d1934ab
znss1989 <znss1989@gmail.com>
2018-06-20 11:36:08 +0800

    enable get, put & delete

--- app.js
+++ app.js
@@ -22,6 +22,37 @@ app.post('/api/genres', (req, res) => {
   res.send(genre);
 });
 
+app.get('/api/genres', (req, res) => {
+  res.send(genres);
+});
+
+app.get('/api/genres/:id', (req, res) => {
+  const genre = genres.find(g => g.id === req.params.id);
+  if (!genre) return res.status(404).send("Genre not found.");
+
+  res.send(genre);
+});
+
+app.put('/api/genres/:id', (req, res) => {
+  const genre = genres.find(g => g.id === req.params.id);
+  if (!genre) return res.status(404).send("Genre not found.");
+
+  const {error} = utils.validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
+app.delete('/api/genres/:id', (req, res) => {
+  const genre = genres.find(g => g.id === req.params.id);
+  if (!genre) return res.status(404).send("Genre not found.");
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+  res.send(genre);
+});
+
 const port = proces.env.port || 3000;
 app.listen(port, () => {
   console.log(`Listening on port: ${port}...`);
