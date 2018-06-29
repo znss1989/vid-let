4870ba05cc88e283489e49412ca48f30197ab431
znss1989 <znss1989@gmail.com>
2018-06-29 11:12:31 +0800

    refactor routes to atomize

--- app.js
+++ app.js
@@ -1,57 +1,13 @@
 const express = require('express');
 
-const genres = require('./genres');
-const utils = require('./utils');
+const index = require('./routes/index');
+const genres = require('./routes/genres');
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send("Welcome to Vid-Let");
-});
-
-app.post('/api/genres', (req, res) => {
-  const {error} = utils.validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-  
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name
-  };
-  genres.push(genre);
-  res.send(genre);
-});
-
-app.get('/api/genres', (req, res) => {
-  res.send(genres);
-});
-
-app.get('/api/genres/:id', (req, res) => {
-  const genre = genres.find(g => g.id === req.params.id);
-  if (!genre) return res.status(404).send("Genre not found.");
-
-  res.send(genre);
-});
-
-app.put('/api/genres/:id', (req, res) => {
-  const genre = genres.find(g => g.id === req.params.id);
-  if (!genre) return res.status(404).send("Genre not found.");
-
-  const {error} = utils.validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-  
-  genre.name = req.body.name;
-  res.send(genre);
-});
-
-app.delete('/api/genres/:id', (req, res) => {
-  const genre = genres.find(g => g.id === req.params.id);
-  if (!genre) return res.status(404).send("Genre not found.");
-
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-  res.send(genre);
-});
+app.use('/', index);
+app.use('/api/genres', genres);
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
--- genres.js
+++
@@ -1,7 +0,0 @@
-const genres = [
-  {id: 1, name: "comedy"},
-  {id: 2, name: "history"},
-  {id: 3, name: "action"},
-];
-
-module.exports = genres;
