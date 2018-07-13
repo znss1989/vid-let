e9ac84b813c4060fd69054fdb1666706dd95e1d7
znss1989 <znss1989@gmail.com>
2018-07-13 10:08:42 +0800

    customers restful with mongo

--- routes/customers.js
+++ routes/customers.js
@@ -1,5 +1,66 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const utils = require('../utils');
+
+const customerSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+  isGold: Boolean
+});
+
+const Customer = mongoose.model('Customer', customerSchema);
 
 const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const {error} = utils.validateCustomer(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const {name, phone='', isGold} = req.body;
+  let customer = new Customer({
+    name,
+    phone,
+    isGold
+  });
+  customer = await customer.save();
+});
+
+router.get('/', async (req, res) => {
+  const customers = await Customer
+    .find();
+  res.send(customers);
+});
+
+router.get('/:id', async (req, res) => {
+  const customer = await Customer.findById(req.params.id);
+
+  if (!customer) return res.status(404).send("Customer not found.");
+  res.send(customer);
+})
+
+router.put('/:id', async (req, res) => {
+  const {error} = utils.validateCustomer(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const {name, phone='', isGold} = req.body;
+  const customer = await Customer.findByIdAndUpdate(req.body.id, {
+    name,
+    phone,
+    isGold
+  }, {
+    new: true
+  });
+
+  if (!customer) return res.status(404).send("Customer not found.");
+  res.send(customer);
+});
+
+router.delete('/:id', async (req, res) => {
+  const customer = await Customer.findByIdAndRemove(req.params.id);
+
+  if (!customer) return res.status(404).send("Customer not found");
+  res.send(customer)
+});
 
 module.exports = router;
--- utils.js
+++ utils.js
@@ -2,12 +2,22 @@ const Joi = require('joi');
 
 const validateGenre = (input) => {
   const scheme = {
-    name: Joi.string().min(3).required()
+    name: Joi.string().min(2).max(50).required()
+  };
+  return Joi.validate(input, scheme);
+};
+
+const validateCustomer = (input) => {
+  const scheme = {
+    name: Joi.string().min(2).max(127).required(),
+    phone: Joi.string().min(6).max(15),
+    isGold: Joi.boolean().required()
   };
   return Joi.validate(input, scheme);
 };
 
 module.exports = {
-  validateGenre
+  validateGenre,
+  validateCustomer
 };
 
